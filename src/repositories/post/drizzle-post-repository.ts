b8455b43cd10947fd91824dda46984c929de9d9c
Lucas Mendes <luckajw@gmail.com>
2025-10-27 02:03:56 -0300

    gerado repositorio para lidar com DB com drizzle

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -8,6 +8,38 @@ export class DrizzlePostRepository implements PostRepository {
       orderBy: (posts, { desc }) => desc(posts.createdAt),
       where: (posts, { eq }) => eq(posts.published, true),
     });
+
+    return posts;
+  }
+
+  async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq, and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
+
+    if (!post)
+      throw new Error(`Publicação não encontrada para o slug: ${slug}`);
+
+    return post;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    });
+
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+
+    if (!post) throw new Error(`Publicação não encontrada para o slug: ${id}`);
+
+    return post;
   }
 }
 
